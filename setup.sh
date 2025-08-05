38cba67790c15bd56b466243c77ed2543857ddb1
Espejo <99065471+eepsjo@users.noreply.github.com>
2025-08-05 20:00:08 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,4 @@ apk update
 apk add sing-box@testing
 rm -rf /var/cache/apk/*
 
-wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -O /usr/bin/cloudflared
-chmod +x /usr/bin/cloudflared
-
 exec /app/launch.sh
